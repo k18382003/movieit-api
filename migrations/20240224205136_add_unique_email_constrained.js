f267949a35d82099c899b4c47f0dea87a7508a41
鄧莉瀕 <k18382003@gmail.com>
2024-02-24 12:55:13 -0800

    Modifying table

---
+++ migrations/20240224205136_add_unique_email_constrained.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('user', function (table) {
+    table.string('email').unique().notNullable().alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('user', function (table) {
+    table.dropUnique('email');
+  });
+};
---
+++ migrations/20240224205232_add_display_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('profile', (table) => {
+    table.string('displayname').unique().notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('profile', (table) => {
+    table.dropColumn('displayname');
+  });
+};
