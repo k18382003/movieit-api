222508a615d7be81f5827bdaff339c76231baf58
鄧莉瀕 <k18382003@gmail.com>
2024-02-24 13:14:55 -0800

    Adding migration add username

---
+++ migrations/20240224211158_add_user_name_column.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table('profile', (table) => {
+    table.string('username').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table('profile', (table) => {
+    table.dropColumn('username');
+  });
+};
