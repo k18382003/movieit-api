60fa601fe3672a5cb5afafe5758d2f5b043320fb
鄧莉瀕 <k18382003@gmail.com>
2024-02-27 08:41:11 -0800

    Selecting photo_url

--- controllers/participant-controller.js
+++ controllers/participant-controller.js
@@ -2,12 +2,11 @@ const knex = require('knex')(require('../knexfile'));
 
 const fetchParticipants = async (req, res) => {
   const participants = await knex('participants as pt')
-    .select('pt.event_id','pr.displayname', 'pt.user_id')
+    .select('pt.event_id', 'pr.displayname', 'pt.user_id', 'pr.photo_url')
     .join('profile as pr', 'pt.user_id', 'pr.user_id')
     .where({
       event_id: req.params.eventId,
     });
-
   return res.status(200).json(participants);
 };
 
